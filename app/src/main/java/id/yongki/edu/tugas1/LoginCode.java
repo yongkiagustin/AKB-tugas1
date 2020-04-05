78a1b939d88fda7fd18d5c045c90d7485bc5699e
yongkiagustin <yongkiagustin92@gmail.com>
2020-04-05 18:26:35 +0700

    update
    set intent mulai

--- app/src/main/java/id/yongki/edu/tugas1/LoginCode.java
+++ app/src/main/java/id/yongki/edu/tugas1/LoginCode.java
@@ -11,4 +11,5 @@ public class LoginCode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_code);
     }
+
 }
--- app/src/main/java/id/yongki/edu/tugas1/MainActivity.java
+++ app/src/main/java/id/yongki/edu/tugas1/MainActivity.java
@@ -5,10 +5,15 @@ package id.yongki.edu.tugas1;
 //DESKRIPSI PEKERJAAN :
 //- MINGGU 5 APRIL 2020 16:40 LAYOUTING MAIN_ACTIVITY
 //- MINGGU 5 APRIL 2020 16:54 MEMBUAT TOMBOL MULAI
+//- MINGGU 5 APRIL 2020 17:32 LAYOUTING LOGIN CODE ACTIVITY
+//- MINGGU 5 APRIL 2020 18:24 SETTING NO ACTION BAR
+//- MINGGU 5 APRIL 2020 17:44 add INTENT mulai
 //
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mulai(View v){
+        Intent intent = new Intent(this, LoginCode.class);
+        startActivity(intent);
+
     }
 }
