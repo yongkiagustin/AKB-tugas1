37a2d0bbc9311289e9ab53b96866615ec109d844
yongkiagustin <yongkiagustin92@gmail.com>
2020-04-05 17:38:13 +0700

    upload pertama

---
+++ app/src/main/java/id/yongki/edu/tugas1/LoginCode.java
@@ -0,0 +1,14 @@
+package id.yongki.edu.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_code);
+    }
+}
---
+++ app/src/main/java/id/yongki/edu/tugas1/MainActivity.java
@@ -0,0 +1,20 @@
+package id.yongki.edu.tugas1;
+//NIM : 10117903
+//NAMA : YONGKI AGUSTIN
+//KELAS : IF6K
+//DESKRIPSI PEKERJAAN :
+//- MINGGU 5 APRIL 2020 16:40 LAYOUTING MAIN_ACTIVITY
+//- MINGGU 5 APRIL 2020 16:54 MEMBUAT TOMBOL MULAI
+//
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
