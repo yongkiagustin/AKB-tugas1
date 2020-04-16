7938c59690cab8d2dc00dec6805bdf455d9338dd
yongkiagustin <yongkiagustin92@gmail.com>
2020-04-16 22:23:15 +0700

    last

--- app/src/main/java/id/yongki/edu/tugas1/LoginCode.java
+++ app/src/main/java/id/yongki/edu/tugas1/LoginCode.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginCode extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "id.yongki.edu.tugas1.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,9 @@ public class LoginCode extends AppCompatActivity {
 
     public void masuk_btn(View v) {
         Intent intent = new Intent(this, biodata.class);
+        EditText editText =(EditText) findViewById(R.id.code_edittext);
+        String ambilCode = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,ambilCode);
         startActivity(intent);
 
     }
--- app/src/main/java/id/yongki/edu/tugas1/biodata.java
+++ app/src/main/java/id/yongki/edu/tugas1/biodata.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class biodata extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "id.yongki.edu.tugas1.MESSAGE";
@@ -14,6 +15,11 @@ public class biodata extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biodata);
+        Intent intent = getIntent();
+        String code = intent.getStringExtra(EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.placeCode_textView);
+        textView.setText(code);
     }
 
     public void selanjutnya_btn(View view) {
